6eb6313b8a352f7a995256e264e4e852ca82ff1a
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-05-19 21:22:32 -0600

    Fix EPDIY board source redefinition in Arduino build

---
+++ src/epdiy_board_lilygo_t5_47.cpp
@@ -0,0 +1,5 @@
+#if defined(ARDUINO) && !defined(PAPERGO_DISABLE_EMBEDDED_EPDIY)
+extern "C" {
+#include "../lib/epdiy/src/board/epd_board_lilygo_t5_47.c"
+}
+#endif
---
+++ src/epdiy_board_v7.cpp
@@ -0,0 +1,5 @@
+#if defined(ARDUINO) && !defined(PAPERGO_DISABLE_EMBEDDED_EPDIY)
+extern "C" {
+#include "../lib/epdiy/src/board/epd_board_v7.c"
+}
+#endif
