0ac03bb4ee4fe037e76d2bef73d9b63ff51fe03a
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-05-19 18:52:44 -0600

    Strengthen epdiy linkage regression test with file-existence checks

---
+++ include/epdiy.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../lib/epdiy/src/epdiy.h"
---
+++ test/test_display_driver_linkage/test_main.cpp
@@ -0,0 +1,37 @@
+#include <unity.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+static std::string readFile(const char* path) {
+  std::ifstream in(path);
+  std::stringstream ss;
+  ss << in.rdbuf();
+  return ss.str();
+}
+
+void test_board_hal_includes_epdiy_header() {
+  const std::string boardHal = readFile("src/BoardHAL.cpp");
+  TEST_ASSERT_TRUE(boardHal.find("#include <epdiy.h>") != std::string::npos);
+}
+
+void test_include_and_root_epdiy_shims_exist_for_arduino_builds() {
+  const std::string includeShim = readFile("include/epdiy.h");
+  const std::string rootShim = readFile("epdiy.h");
+  TEST_ASSERT_TRUE(includeShim.find("../lib/epdiy/src/epdiy.h") != std::string::npos);
+  TEST_ASSERT_TRUE(rootShim.find("include/epdiy.h") != std::string::npos);
+
+  std::ifstream includeHdr("include/epdiy.h");
+  std::ifstream rootHdr("epdiy.h");
+  std::ifstream vendorHdr("lib/epdiy/src/epdiy.h");
+  TEST_ASSERT_TRUE(includeHdr.good());
+  TEST_ASSERT_TRUE(rootHdr.good());
+  TEST_ASSERT_TRUE(vendorHdr.good());
+}
+
+int main(int argc, char** argv) {
+  UNITY_BEGIN();
+  RUN_TEST(test_board_hal_includes_epdiy_header);
+  RUN_TEST(test_include_and_root_epdiy_shims_exist_for_arduino_builds);
+  return UNITY_END();
+}
